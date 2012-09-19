6e78f9aec5236b1d1feb672861b8738d91b36b1d
Jacky Chan <libing.chen@gmail.com>
2012-09-19 11:28:47 +0800

    S3 Shell banner provider

---
+++ src/main/java/org/mvnsearch/aws/s3/spring/shell/commands/S3ShellBannerProvider.java
@@ -0,0 +1,73 @@
+package org.mvnsearch.aws.s3.spring.shell.commands;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.shell.core.CommandMarker;
+import org.springframework.shell.core.annotation.CliCommand;
+import org.springframework.shell.plugin.support.DefaultBannerProvider;
+import org.springframework.shell.support.util.StringUtils;
+import org.springframework.stereotype.Component;
+
+/**
+ * S3 Shell banner provider
+ *
+ * @author linux_china
+ */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class S3ShellBannerProvider extends DefaultBannerProvider implements CommandMarker {
+    /**
+     * get CLI banner
+     *
+     * @return banner text
+     */
+    @SuppressWarnings("StringConcatenationInsideStringBufferAppend")
+    @CliCommand(value = {"version"}, help = "Displays current shell version")
+    public String getBanner() {
+        StringBuilder buf = new StringBuilder();
+        buf.append("=======================================" + StringUtils.LINE_SEPARATOR);
+        buf.append("*                                     *" + StringUtils.LINE_SEPARATOR);
+        buf.append("*            AWS S3 Shell             *" + StringUtils.LINE_SEPARATOR);
+        buf.append("*                                     *" + StringUtils.LINE_SEPARATOR);
+        buf.append("=======================================");
+        return buf.toString();
+    }
+
+    /**
+     * display author information
+     *
+     * @return author information
+     */
+    @CliCommand(value = {"author"}, help = "Displays author information")
+    public String author() {
+        return "Jacky Chan <linux_china@hotmail.com>, Please follow me: http://weibo.com/linux2china";
+    }
+
+    /**
+     * current version
+     *
+     * @return version
+     */
+    public String getVersion() {
+        return "1.0.0";
+    }
+
+    /**
+     * welcome message
+     *
+     * @return welcome message
+     */
+    public String getWelcomeMessage() {
+        return "Welcome to S3 Shell! Version: " + getVersion();
+    }
+
+    /**
+     * commander name
+     *
+     * @return name
+     */
+    @Override
+    public String name() {
+        return "aliyun-oss-java-cli";
+    }
+}
